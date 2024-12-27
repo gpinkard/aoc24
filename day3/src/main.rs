f7a48e3502d0eb8f27cc476fc115694631b1462d
Gabriel Pinkard <gabriel.pinkard@walmart.com>
2024-12-27 08:39:25 -0800

    day 3 sol 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -7,35 +7,47 @@ const INPUT_FILE: &str = "input.txt";
 fn main() {
     match parse_input() {
         Ok(data) => {
-            solution_one(&data);
-            // solution_two(&data);
+            match solution_one(&data) {
+                Ok(total) => println!("solution one: {total}"),
+                Err(e) => log_err_and_exit("error running solution_one algorithm", e),
+            }
+            match solution_two(&data) {
+                Ok(total) => println!("solution two: {total}"),
+                Err(e) => log_err_and_exit("error running solution_two algorithm", e),
+            }
         }
-        Err(e) => {
-            println!("error parsing input: {e}");
-            process::exit(1);
-        }
+        Err(e) => log_err_and_exit("unable to parse input file", e),
     }
 }
 
-fn solution_one(data: &str) {
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+fn solution_one(data: &str) -> Result<i32, Box<dyn Error>> {
+    let re = Regex::new(r"(?s)mul\((\d+),(\d+)\)")?;
     let mut total = 0;
 
-    for (_, [n1, n2]) in re.captures_iter(data).map(|elem| elem.extract()) {
-        total += n1.parse::<i32>().unwrap() * n2.parse::<i32>().unwrap();
+    for (_, [n1, n2]) in re.captures_iter(data).map(|val| val.extract()) {
+        total += n1.parse::<i32>()? * n2.parse::<i32>()?;
     }
 
-    println!("total: {total}");
+    Ok(total)
 }
 
-/*
-fn solution_two(data: &String) {
-    let re = Regex::new("").unwrap();
-    println!("data: {data}");
+fn solution_two(data: &str) -> Result<i32, Box<dyn Error>> {
+    let re = Regex::new(r"(?s)(^|do\(\)).*?(don't\(\)|$)")?;
+    let mut total = 0;
+
+    for (x, [_do, _dont]) in re.captures_iter(data).map(|val| val.extract()) {
+        total += solution_one(x)?
+    }
+
+    Ok(total)
 }
-*/
 
 fn parse_input() -> Result<String, Box<dyn Error>> {
     let data = fs::read_to_string(INPUT_FILE)?;
     Ok(data)
+}
+
+fn log_err_and_exit(message: &str, err: Box<dyn Error>) {
+    println!("{message}: {err}");
+    process::exit(1);
 }
